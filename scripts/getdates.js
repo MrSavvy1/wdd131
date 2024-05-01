0701a0d0aa83d90a45a412cfa222cb640eb0d757
Saviour Edethu <108595932+MrSavvy1@users.noreply.github.com>
2024-05-01 02:12:29 +0100

    Update getdates.js 2nd

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear(); 
     const lastModified = document.lastModified; 
-    document.getElementById('currentYear').textContent += currentYear;
+    document.getElementById('currentyear').textContent += currentYear;
     document.getElementById('lastModified').textContent += lastModified;
 });
